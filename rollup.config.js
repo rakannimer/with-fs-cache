db16a8bf36a6996b212e3eae3539284cbb109b90
rakannimer <rakannimer@gmail.com>
2018-08-19 10:34:42 +0300

    Rename .cjs.js to .js in build

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,12 @@ import typescript from "rollup-plugin-typescript2";
 export default {
   input: "src/index.ts",
   output: [
-    // {
-    //   file: "dist/index.umd.js",
-    //   format: "umd",
-    //   name: "ReactGoogleCharts"
-    // },
     {
       file: "dist/index.esm.js",
       format: "esm"
     },
     {
-      file: "dist/index.cjs.js",
+      file: "dist/index.js",
       format: "cjs"
     }
   ],
